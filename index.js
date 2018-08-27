65dc24e6e9da8a7d8b4080784ce14efb045041bf
Mat34544 <40038662+Mat34544@users.noreply.github.com>
2018-08-26 21:47:31 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} en Linea!`);
-  bot.user.setActivity("Programando...");
+  bot.user.setActivity("Miembros M community", {type: "VIGILANDO"});
 });
 
 bot.on("message", async message => {
